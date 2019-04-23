0da3d3d910d52671be6e5bf587d72c008613419c
王明 <wmltyq@gmail.com>
2019-04-23 16:50:27 +0800

    Update models.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class BlogArticles(models.Model):
 	title = models.CharField(max_length=300)
-	author = models.ForeignKey(User, related_name='blog_posts')
+	author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
 	body = models.TextField()
 	publish = models.DateTimeField(default=timezone.now)
 
